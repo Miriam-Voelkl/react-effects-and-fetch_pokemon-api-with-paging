cecc6bde90af0a81694f3f9885fcba0131df135d
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-11 15:07:00 +0100

    add functionality to prev and next buttons

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,27 +2,45 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${currentPage}`
         );
         const data = await response.json();
         setPokemon(data.results);
+        setData(data);
+        console.log(data.results);
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
+  }, [currentPage]);
+
+  function handlePreviousButton() {
+    setCurrentPage(currentPage - 20);
+  }
+
+  function handleNextButton() {
+    setCurrentPage(currentPage + 20);
+  }
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      {data.previous !== 0 ?? (
+        <button type="button" onClick={handlePreviousButton}>
+          Previous Page
+        </button>
+      )}
+      <button type="button" onClick={handleNextButton}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
